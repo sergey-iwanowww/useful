4c7718ff11709642d0c3270dd498b93164310542
sergey.iwanowww <sergey.iwanowww@gmail.com>
2023-03-13 00:19:28 +0700

    useful comment was added into auth2-pre-script.js

--- postman/auth2-pre-script.js
+++ postman/auth2-pre-script.js
@@ -1,3 +1,6 @@
+// adaptation for auth2 and application/x-www-form-urlencoded of this pre-script:
+// https://gist.github.com/bcnzer/073f0fc0b959928b0ca2b173230c0669
+
 const echoPostRequest = {
   url: pm.variables.get('tokenUrl'),
   method: 'POST',
